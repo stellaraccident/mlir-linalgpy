e51838352cb6cd0d540cfe7d751f72955ad3b2c4
Stella Laurenzo <stellaraccident@gmail.com>
2021-02-17 15:17:35 -0800

    Strip down the mm sample for upstream.

--- samples/mm.py
+++ samples/mm.py
@@ -4,23 +4,19 @@ from mlir_linalg.dsl.yaml_helper import *
 
 
 @tc_def_op
-def matmul(A=TensorDef(T, S.M, S.K),
-           B=TensorDef(T, S.K, S.N),
-           C=TensorDef(T, S.M, S.N, output=True)):
-  """Non accumulator polymorphic version."""
-  C[D.m, D.n] += A[D.m, D.k] * B[D.k, D.n]
-
-
-@tc_def_op
-def matmul_poly(A=TensorDef(T, S.M, S.K),
+def test_matmul(A=TensorDef(T, S.M, S.K),
                 B=TensorDef(T, S.K, S.N),
                 C=TensorDef(U, S.M, S.N, output=True)):
+  """Type polymorphic matrix multiplication.
+
+  This op is presently here to test a new path for generation and will replace
+  the existing 'matmul' op when ready. Do not use.
+  """
   C[D.m, D.n] += A[D.m, D.k] * B[D.k, D.n]
 
 
 # TODO: Make a neat little helper to interpret an op module as
-# yaml linal ops.
+# yaml linalg ops.
 configs = []
-configs.extend(LinalgOpConfig.from_tc_op_def(matmul.model))
-configs.extend(LinalgOpConfig.from_tc_op_def(matmul_poly.model))
+configs.extend(LinalgOpConfig.from_tc_op_def(test_matmul.model))
 print(yaml_dump_all(configs))
