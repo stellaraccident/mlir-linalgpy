bd0ab79fd78524edd311b92184c3deee45080329
Stella Laurenzo <stellaraccident@gmail.com>
2021-02-20 22:40:11 -0800

    Rename to polymorphic_matmul.

--- samples/mm.py
+++ samples/mm.py
@@ -4,9 +4,9 @@ from mlir_linalg.dsl.yaml_helper import *
 
 
 @tc_def_op
-def test_matmul(A=TensorDef(T, S.M, S.K),
-                B=TensorDef(T, S.K, S.N),
-                C=TensorDef(U, S.M, S.N, output=True)):
+def polymorphic_matmul(A=TensorDef(T, S.M, S.K),
+                       B=TensorDef(T, S.K, S.N),
+                       C=TensorDef(U, S.M, S.N, output=True)):
   """Type polymorphic matrix multiplication.
 
   This op is presently here to test a new path for generation and will replace
@@ -18,5 +18,5 @@ def test_matmul(A=TensorDef(T, S.M, S.K),
 # TODO: Make a neat little helper to interpret an op module as
 # yaml linalg ops.
 configs = []
-configs.extend(LinalgOpConfig.from_tc_op_def(test_matmul.model))
+configs.extend(LinalgOpConfig.from_tc_op_def(polymorphic_matmul.model))
 print(yaml_dump_all(configs))
